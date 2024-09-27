be303def8ff56d51232d332f7a53a825444df614
jhonatanbsouza <135228782+jhonatanbsouza@users.noreply.github.com>
2024-09-27 08:55:36 -0300

    27/09
    async function visualizaDadosGlobais(){
        const res = await fetch(url);
        const dados = await res.json();
        console.log(dados);
    
        const paragrafo = document.createElement('p')
        // * Cria uma tag <p> que é um paragrafo (no HTML)
        paragrafo.classList.add('graficos-texto')
        // * Cria uma classe 'graficos-texto' na tag <p>
        const container = document.getElementById('graficos-container')
    }

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,3 +1,15 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-console.log(url);
+async function visualizaDadosGlobais(){
+    const res = await fetch(url);
+    const dados = await res.json();
+    console.log(dados);
+
+    const paragrafo = document.createElement('p')
+    // * Cria uma tag <p> que é um paragrafo (no HTML)
+    paragrafo.classList.add('graficos-texto')
+    // * Cria uma classe 'graficos-texto' na tag <p>
+    const container = document.getElementById('graficos-container')
+}
+
+visualizaDadosGlobais();
