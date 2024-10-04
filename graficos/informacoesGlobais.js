0a7844f3f2bdfab8a64a3ea89d0ca59852478c21
jhonatanbsouza <135228782+jhonatanbsouza@users.noreply.github.com>
2024-10-04 08:55:53 -0300

    04/10
    Criação de estilizações para o texto, <span> e para o rodapé

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,6 +3,9 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizaDadosGlobais(){
     const res = await fetch(url);
     const dados = await res.json();
+    const totalPessoasMundo = (dados.total_pessoas_mundo)/1e9;
+    const totalPessoasConectadas = (dados.total_pessoas_conectadas)/1e9;
+    const tempoMedio = dados.tempo_medio;
     console.log(dados);
 
     const paragrafo = document.createElement('p')
@@ -10,6 +13,10 @@ async function visualizaDadosGlobais(){
     paragrafo.classList.add('graficos-texto')
     // * Cria uma classe 'graficos-texto' na tag <p>
     const container = document.getElementById('graficos-container')
+    paragrafo.innerHTML = `O mundo tem <span> ${totalPessoasMundo} bilhões </span> de 
+    habitantes, dos quais <span> ${totalPessoasConectadas} bilhões </span> estão conectados
+    em alguma rede social por um tempo médio de <span> ${tempoMedio} horas </span>.`
+    container.appendChild(paragrafo);
 }
 
 visualizaDadosGlobais();
