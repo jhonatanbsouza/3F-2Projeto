f10f3aa4e5f4a5c8c2008b5a766f1cb9726dea5d
jhonatanbsouza <135228782+jhonatanbsouza@users.noreply.github.com>
2024-10-11 10:53:04 -0300

    Add files via upload

--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -13,4 +13,11 @@ async function quantidadeDeUsuarios(){
             type: 'bar'
         }
     ]
+
+    const grafico = document.createElement('div');
+    grafico.className = 'grafico';
+    document.getElementById('graficos-container').appendChild(grafico);
+    Plotly.newPlot(grafico,infos);
 }
+
+quantidadeDeUsuarios();
